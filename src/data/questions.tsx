e4a4813c01026252e508f0b1cdb14e9deeff593c
Pedro Luca Prates <pedrolucaofc@outlook.com>
2024-04-12 16:41:31 -0300

    feat: add new questions to the tsx data file

--- src/data/questions.tsx
+++ src/data/questions.tsx
@@ -1,12 +1,247 @@
 export const questions = [
   {
-    title: "What is the capital of France?",
+    title: "Qual é a capital da França?",
     indexCorrect: 0,
-    alternativa: ["Paris", "London", "Berlin", "Madrid"]
+    alternativa: ["Paris", "Londres", "Berlin", "Madrid"]
   },
   {
-    title: "What is the capital of England?",
-    indexCorrect: 1,
-    alternativa: ["Paris", "London", "Berlin", "Madrid"]
+    title: "Qual é a capital da Inglaterra?",
+    indexCorrect: 3,
+    alternativa: ["Brasília", "Orlando", "Nova Iorque", "Londres"]
   },
+  {
+    title: "Qual a capital do Brasil?",
+    indexCorrect: 0,
+    alternativa: ["Brasília", "São Paulo", "Rio de Janeiro", "Belo Horizonte"]
+  },
+  {
+    title: "Qual a capital dos Estados Unidos?",
+    indexCorrect: 0,
+    alternativa: ["Washington, D.C.", "Nova York", "Los Angeles", "Chicago"]
+  },
+  {
+    title: "Qual a maior floresta tropical do mundo?",
+    indexCorrect: 0,
+    alternativa: ["Floresta Amazônica", "Floresta Congolesa", "Floresta do Sudeste Asiático", "Mata Atlântica"]
+  },
+  {
+    title: "Qual a moeda oficial do Brasil?",
+    indexCorrect: 0,
+    alternativa: ["Real", "Dólar americano", "Euro", "Libra esterlina"]
+  },
+  {
+    title: "Qual a obra-prima de arte renascentista pintada por Leonardo da Vinci?",
+    indexCorrect: 0,
+    alternativa: ["Mona Lisa", "A Última Ceia", "A Primavera", "O Nascimento de Vênus"]
+  },
+  {
+    title: "Qual a Torre mais famosa do mundo, localizada em Paris?",
+    indexCorrect: 0,
+    alternativa: ["Torre Eiffel", "Big Ben", "Empire State Building", "Torre de Pisa"]
+  },
+  {
+    title: "Qual a invenção revolucionou a comunicação a distância no século XIX?",
+    indexCorrect: 0,
+    alternativa: ["Telefone", "Rádio", "Televisão", "Computador"]
+  },
+  {
+    title: "Qual o nome do buscador de internet mais utilizado do mundo?",
+    indexCorrect: 0,
+    alternativa: ["Google", "Bing", "Yahoo", "DuckDuckGo"]
+  },
+  {
+    title: "Qual a capital do estado de São Paulo?",
+    indexCorrect: 0,
+    alternativa: ["São Paulo", "Campinas", "Santos", "Ribeirão Preto"]
+  },
+  {
+    title: "Qual a capital do estado do Rio de Janeiro?",
+    indexCorrect: 0,
+    alternativa: ["Rio de Janeiro", "Niterói", "Petrópolis", "Angra dos Reis"]
+  },
+  {
+    title: "Qual a capital do estado de Minas Gerais?",
+    indexCorrect: 0,
+    alternativa: ["Belo Horizonte", "Uberlândia", "Juiz de Fora", "Contagem"]
+  },
+  {
+    title: "Qual a data em que se comemora o Dia da Independência do Brasil?",
+    indexCorrect: 0,
+    alternativa: ["7 de setembro", "15 de novembro", "21 de abril", "12 de outubro"]
+  },
+  {
+    title: "Qual a importância do marketing para o sucesso de um negócio?",
+    indexCorrect: 1,
+    alternativa: ["É apenas uma forma de divulgar a empresa.", "É fundamental para atrair clientes, fidelizá-los e aumentar as vendas.", "É um custo desnecessário para empresas iniciantes.", "O marketing boca a boca é suficiente para alcançar novos clientes."]
+  },
+  {
+    title: "Como superar os desafios e alcançar o sucesso como empreendedor(a)?",
+    indexCorrect: 2,
+    alternativa: ["Ter sorte e esperar o momento certo.", "Desistir ao encontrar os primeiros obstáculos.", "Ser resiliente, persistente e buscar constante aprendizado.", "Copiar as estratégias de negócios de empresas bem-sucedidas."]
+  },
+  {
+    title: "Qual o continente onde se localiza o Brasil?",
+    indexCorrect: 1,
+    alternativa: ["Europa", "América do Sul", "Ásia", "África"]
+  },
+  {
+    title: "Qual o planeta em que vivemos?",
+    indexCorrect: 2,
+    alternativa: ["Marte", "Vênus", "Terra", "Júpiter"]
+  },
+  {
+    title: "O que é cultura?",
+    indexCorrect: 1,
+    alternativa: ["Um conjunto de regras que devemos seguir.", "Um conjunto de costumes, crenças e valores que um grupo de pessoas compartilha.", "Um tipo de planta que cresce no jardim.", "Um animal que vive na floresta."]
+  },
+  {
+    title: "O que é arte?",
+    indexCorrect: 1,
+    alternativa: ["Uma brincadeira que inventamos com os amigos.", "Uma forma de expressão que usamos para mostrar nossos sentimentos e ideias.", "Uma música que ouvimos no rádio.", "Uma roupa que usamos em ocasiões especiais."]
+  },
+  {
+    title: "Quais são alguns exemplos de arte?",
+    indexCorrect: 1,
+    alternativa: ["Brincar de esconde-esconde com os amigos.", "Desenhar, pintar, cantar e dançar.", "Assistir vídeos engraçados na internet.", "Jogar videogame com os amigos."]
+  },
+  {
+    title: "Qual é o nome do jogo de tabuleiro mais popular do mundo?",
+    indexCorrect: 0,
+    alternativa: ["War", "Banco imobiliário", "Ludo", "Xadrez"]
+  },
+  {
+    title: "Qual é o nome do esporte mais popular do mundo?",
+    indexCorrect: 1,
+    alternativa: ["Baquete", "Futebol", "Vôlei", "Tênis"]
+  },
+  {
+    title: "Qual o nome do artista que pintou a Mona Lisa?",
+    indexCorrect: 3,
+    alternativa: ["Michelangelo", "Rafael Sanzio", "Tarcila do Amaral", "Leonardo Da Vinci"]
+  },
+  {
+    title: "Qual o nome do autor que escreveu “Dom Casmurro”?",
+    indexCorrect: 1,
+    alternativa: ["José de Alencar", "Machado de Assis", "Jorge Amado", "Vicent Van Gogh"]
+  },
+  {
+    title: "Qual jogador de futebol que ganhou a Bola de Ouro em 2023?",
+    indexCorrect: 3,
+    alternativa: ["Neymar Júnior", "Cristiano Ronaldo", "Lionel Messi", "Kylian Mbappé"]
+  },
+  {
+    title: "Qual planeta mais próximo do sol?",
+    indexCorrect: 1,
+    alternativa: ["Terra", "Mercúrio", "Marte", "Júpter"]
+  },
+  {
+    title: "Qual o nome do vírus que causou a pandemia da covid-19?",
+    indexCorrect: 3,
+    alternativa: ["Monkeypox", "Influenza", "Marburg", "SARS-CoV-2"]
+  },
+  {
+    title: "É um prato típico da cidade de Salvador-Bahia:",
+    indexCorrect: 2,
+    alternativa: ["Carne de Sol", "Tacacá", "Acarajé", "Tapioca"]
+  },
+  {
+    title: "A primeira capital do Brasil foi uma cidade do Nordeste. Qual?",
+    indexCorrect: 0,
+    alternativa: ["Salvador", "João Pessoa", "São Luiz", "Recife"]
+  },
+  {
+    title: "Qual o nome da artista que pintou o quadro Abaporu e ficou muito reconhecida na semana de arte moderna de 22?",
+    indexCorrect: 2,
+    alternativa: ["Zina Aita", "Anita Mafalti", "Tarsila do Amaral", "Guiomar Novais"]
+  },
+  {
+    title: "O que é diversidade cultural?",
+    indexCorrect: 1,
+    alternativa: ["Valorizar somente sua cultura", "Ter uma variedade de culturas em uma área", "Ter somente uma cultura", "Não ter nenhum tipo de cultura"]
+  },
+  {
+    title: "Em uma loja de informática, Paulo comprou: um computador no valor de 2200 reais, uma impressora por 700 reais e dois cartuchos que custam 50 reais cada um. Os objetos foram pagos em 5 parcelas iguais. O valor de cada parcela, em reais, foi igual a:",
+    indexCorrect: 1,
+    alternativa: ["654", "494", "600", "560"]
+  },
+  {
+    title: "Qual a raiz quadrada de 36?",
+    indexCorrect: 2,
+    alternativa: ["3", "9", "6", "7"]
+  },
+  {
+    title: "Qual a raiz quadrada de 49?",
+    indexCorrect: 1,
+    alternativa: ["8", "7", "5", "6"]
+  },
+  {
+    title: "Quanto é 7 + 3 x 6?",
+    indexCorrect: 3,
+    alternativa: ["30", "20", "25", "60"]
+  },
+  {
+    title: "De 0 a 100, quantos números 3 tem?",
+    indexCorrect: 2,
+    alternativa: ["20", "12", "11", "19"]
+  },
+  {
+    title: "Quanto é 24 ÷ 4 + 10 – 2?",
+    indexCorrect: 2,
+    alternativa: ["12", "20", "16", "14"]
+  },
+  {
+    title: "É uma cantora baiana:",
+    indexCorrect: 1,
+    alternativa: ["Anitta", "Ivete Sangalo", "Ana Castela", "Ludmila"]
+  },
+  {
+    title: "Qual país derrotou o Brasil por 7x1 na copa do mundo de 2014?",
+    indexCorrect: 2,
+    alternativa: ["Rússia", "Espanha", "Alemanha", "Portugal"]
+  },
+  {
+    title: "Quantas copas do mundo o Brasil já ganhou?",
+    indexCorrect: 1,
+    alternativa: ["7", "5", "3", "2"]
+  },
+  {
+    title: "Qual time de futebol ganhou mais copas do mundo?",
+    indexCorrect: 0,
+    alternativa: ["Brasil", "Itália", "Alemanha", "Espanha"]
+  },
+  {
+    title: "É um cantor baiano:",
+    indexCorrect: 2,
+    alternativa: ["Roberto Carlos", "Gustavo Lima", "Léo Santana", "Zezé de Camargo"]
+  },
+  {
+    title: "Quem foi o (a) vencedor (a) do Big Brother Brasil em 2021?",
+    indexCorrect: 2,
+    alternativa: ["Arthur Aguiar", "Amanda", "Juliette", "Thelma"]
+  },
+  {
+    title: "Quem foi o campeão do Brasileirão em 2023?",
+    indexCorrect: 0,
+    alternativa: ["Flamengo", "Palmeiras", "Corinthians", "São Paulo"]
+  },
+  {
+    title: "Quem criou as três leis: Princípio da Inércia, da dinâmica e da ação e reação no século XVII?",
+    indexCorrect: 2,
+    alternativa: ["George Newton", "Frederic Newton", "Isaac Newton", "Robert Newton"]
+  },
+  {
+    title: "A distância entre a cidade de Guanambi e Bom Jesus da Lapa é de aproximadamente 120 km. Um ônibus que sai de Guanambi com um grupo de romeiros às 9h e chega às 11h em Bom Jesus da Lapa, desenvolveu qual velocidade média?",
+    indexCorrect: 0,
+    alternativa: ["60 km/h", "70 km/h", "80 km/h", "90 km/h"]
+  },
+  {
+    title: "É um escritor baiano:",
+    indexCorrect: 0,
+    alternativa: ["Jorge Amado", "Machado de Assis", "Gil Vicente", "Camilo Castelo Branco"]
+  },
+  {
+    title: "Qual nome do escritor dos livros: “Senhora” e “Iracema”?",
+    indexCorrect: 1,
+    alternativa: ["Machado de Assis", "José de Alencar", "Jorge Amado", "Gil Vicente"]
+  }
 ]
